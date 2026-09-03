473cb36cde416947ac0914c39ea41d7f49778476
zaira <iamzaira2@gmail.com>
2026-09-03 16:31:40 +0530

    checks

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+
+dotenv.config() //tells the dotenv package to read your .env file and load those values into Node’s process.env.
+
+const app = express()  //here we made a server
+const Port =8085
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log(DB_URL)
+        console.log("DB connected")
+}).catch((err)=>{
+    console.log(DB_URL)
+    console.log(err)
+
+})
+
+app.use(express.json())
+
+app.use('/users',userRoutes)
+
+app.get('/',(req,res)=>{
+    res.send('hellloo')
+})
+app.get('/s',(req,res)=>{
+    res.send('sss')
+})
+
+app.listen(Port,()=>{
+    console.log(`server started at ${Port}`)
+})
